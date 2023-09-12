15816aa92873ab7b93a0f9aa3608b54ad401341e
balajipasala <balaji.pasala@gmail.com>
2023-09-13 02:00:55 +0530

    setup.py file

---
+++ MyPackage/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyPackage',
+    version='0.1.0',
+    description='My Custom Python Package',
+    author='Balaji.pasala',
+    author_email='balaji.pasala@gmail.com',
+    url='https://github.com/balajipasala/MyPackage',
+    packages=find_packages(),
+    install_requires=[],
+)
